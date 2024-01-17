1e48f1d6c778b149d0e04583caa1a6595a06e55a
eknocklein <eric.knocklein@gmail.com>
2024-01-17 20:09:26 +0100

    extension working?

--- enable.js
+++ enable.js
@@ -1,10 +1,12 @@
 const iframes = document.getElementsByTagName('iframe');
+// debugger;
 for (var i=0; i<iframes.length; i++) {
     var b;
     try {
         b = iframes[i].contentDocument.body;
         console.log(b);
     } catch (e) {
+        console.log(i);
         continue;
     }
     // debugger;
